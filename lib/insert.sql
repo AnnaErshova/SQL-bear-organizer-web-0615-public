45b8e68d8a5bf40d7db680fe28be1ec52c5b2b95
AnnaErshova <anna.ershova@gmail.com>
2015-06-16 09:34:17 -0400

    bears organized

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears VALUES 
+                (1,"Mr. Chocolate", 10, "M", "dark brown", "calm", "FALSE"),
+                (2,"Rowdy", 12, "M", "black", "intense", "TRUE"),
+                (3,"Tabitha", 3, "F", "dark brown", "Nice", "TRUE"),
+                (4,"Sargeant Brown", 19, "M", "Green", "Slimy", "FALSE"),
+                (5,"Melissa", 22, "F", "dark brown", "goofy", "TRUE"),
+                (6,"Grinch", 3, "M", "Black", "Grinchy", "TRUE"),
+                (7,"Wendy", 6, "F", "Blue", "naive", "TRUE"),
+                (8, null, 35, "M", "black", "aggressive", "FALSE");
